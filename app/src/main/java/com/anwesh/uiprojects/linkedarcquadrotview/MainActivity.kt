4a09373ac53270230ffef3d84d08189c1432c201
Anwesh43 <anweshthecool0@gmail.com>
2019-06-24 02:26:08 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedarcquadrotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedarcquadrotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedarcquadrotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.arcquadrotview.ArcQuadRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ArcQuadRotView.create(this)
     }
 }
