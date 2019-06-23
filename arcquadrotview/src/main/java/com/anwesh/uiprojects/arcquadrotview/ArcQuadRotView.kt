7fc83121c07f9ed5cbf5049166905d04dde9efa5
Anwesh43 <anweshthecool0@gmail.com>
2019-06-24 02:20:17 +0530

    adding AQRNode clas

--- arcquadrotview/src/main/java/com/anwesh/uiprojects/arcquadrotview/ArcQuadRotView.kt
+++ arcquadrotview/src/main/java/com/anwesh/uiprojects/arcquadrotview/ArcQuadRotView.kt
@@ -133,4 +133,48 @@ class ArcQuadRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AQRNode(var i : Int, val state : State = State()) {
+
+        private var next : AQRNode? = null
+        private var prev : AQRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = AQRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAQRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AQRNode {
+            var curr : AQRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
