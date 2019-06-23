a06fc3e056ee82fad2bcf7b93a124a8724386050
Anwesh43 <anweshthecool0@gmail.com>
2019-06-24 02:02:26 +0530

    adding libraries and constants

--- arcquadrotview/src/main/java/com/anwesh/uiprojects/arcquadrotview/ArcQuadRotView.kt
+++ arcquadrotview/src/main/java/com/anwesh/uiprojects/arcquadrotview/ArcQuadRotView.kt
@@ -3,3 +3,23 @@ package com.anwesh.uiprojects.arcquadrotview
 /**
  * Created by anweshmishra on 24/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val arcs : Int = 2
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 2.9f
+val scDiv : Double = 0.51
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rotDeg : Float = 360f / arcs
+val finalDeg : Float = 360f
