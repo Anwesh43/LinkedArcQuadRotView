e91568f91167121d87f97869bb9f0aa8534b78d7
Anwesh43 <anweshthecool0@gmail.com>
2019-06-24 02:12:55 +0530

    adding ArcQuadRotView class

--- arcquadrotview/src/main/java/com/anwesh/uiprojects/arcquadrotview/ArcQuadRotView.kt
+++ arcquadrotview/src/main/java/com/anwesh/uiprojects/arcquadrotview/ArcQuadRotView.kt
@@ -69,3 +69,20 @@ fun Canvas.drawAQRNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class ArcQuadRotView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
